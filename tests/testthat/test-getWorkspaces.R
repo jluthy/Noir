d1ab3360ca7e25c1627998152d531dc1c1c72fb6
Josh Luthy <joshl@flowjo.com>
2019-10-10 14:14:19 -0700

    update to docs

--- tests/testthat/test-getWorkspaces.R
+++ tests/testthat/test-getWorkspaces.R
@@ -3,5 +3,5 @@ library(testthat)
 library(Noir)
 
 test_that("getWorkspaces() will give an output message when not connected to API", {
-  expect_output(getWorkspaces(url), "Get Workspaces Generated an Error")
+  expect_output(getWorkspaces(urlWSP), "Get Workspaces Generated an Error")
 })
